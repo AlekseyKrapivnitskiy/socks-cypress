aab8cb2aefdc074b0b6acec98eaaa5dd75a44b97
krapivnitskiy <makartut374@gmail.com>
2022-10-12 11:33:54 +0300

    change url

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,5 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
-describe('My first test', () => {
-  it('Does not match', () => {
-    expect(true).to.equal(true)
+    cy.visit('http://localhost/index.html')
   })
 })
